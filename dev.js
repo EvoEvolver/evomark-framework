41c736a5481afbad301ff473188756bbc346be27
ZiJian Zhang <doomspec@outlook.com>
2021-11-03 18:43:16 -0400

    Add new option

--- dev.js
+++ dev.js
@@ -1,22 +1,26 @@
 var argv = require('minimist')(process.argv.slice(2));
 
 var settings = {
-    src: "./docs/",
+    src: "./docs_src/",
+    mode: ""
 }
 
 Object.assign(settings, argv)
 
 var child_process = require('child_process');
 
-var evomark_complier = child_process.spawn('node', ['./packages/evomark-loader/index.js',"--src",settings.src], { encoding: 'utf-8' });
+if(settings.mode!="nuxt"){
+    var evomark_complier = child_process.spawn('node', ['./packages/evomark-loader/index.js',"--src",settings.src], { encoding: 'utf-8' });
 
-evomark_complier.stdout.on('data', function (chunk) {
-    console.log("[EvoMark] "+chunk.toString());
-});
-evomark_complier.stderr.on('data', (data) => {
-    console.log("[EvoMark] "+data);
-});
+    evomark_complier.stdout.on('data', function (chunk) {
+        console.log("[EvoMark] "+chunk.toString());
+    });
+    evomark_complier.stderr.on('data', (data) => {
+        console.log("[EvoMark] "+data);
+    });
+}
 
+if(settings.mode!="evomark"){
 var nuxt_dev_server = child_process.spawn("yarn",["dev"], { encoding: 'utf-8' ,cwd: 'packages/evomarked-nuxt'});
 
 nuxt_dev_server.stdout.on('data', function (chunk) {
@@ -25,3 +29,4 @@ nuxt_dev_server.stdout.on('data', function (chunk) {
 nuxt_dev_server.stderr.on('data', (data) => {
     console.log("[Nuxt] "+data);
 });
+}
